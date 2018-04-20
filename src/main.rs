dc5d80814b3667ed054a7e2b016db3fe5d87b287
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2018-04-20 12:53:04 -0400

    Add some git2 stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate colored;
+extern crate git2;
+
 use colored::*;
+use git2::{Repository, Error, StatusOptions, ErrorCode, SubmoduleIgnore};
 
 fn main() {
     println!("{}", "Hello, world!".blue().bold().on_color("white"));
